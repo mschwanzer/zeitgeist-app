eff483ae529749fa722bc7582223f699a669e056
Michael Schwanzer <michael.schwanzer@gmail.com>
2013-02-18 19:50:47 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZeitdiceApp::Application.config.secret_token = 'e03f7828c9614379e3c415d0d25df58b65bbedddb3acc9f7fffeb2f1857b6f993255f702ff0ac09af5c637ce387927a18cc6f3c1432ac8eb88ca9585bd78e703'
